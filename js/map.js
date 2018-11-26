d9cbedce562bb9daeed86d5517de3c31c5daaa32
txenart <txen2008@gmail.com>
2018-11-27 01:11:14 +0300

    Добавил объекту свойство hotel.offer.features

--- js/map.js
+++ js/map.js
@@ -1,6 +1,18 @@
 'use strict';
 
-var HOTEL_TITLE = ['Большая уютная квартира',
+var AVATAR_NUMBER = [
+  '01',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+];
+
+var HOTEL_TITLE = [
+  'Большая уютная квартира',
   'Маленькая неуютная квартира',
   'Огромный прекрасный дворец',
   'Маленький ужасный дворец',
@@ -10,22 +22,26 @@ var HOTEL_TITLE = ['Большая уютная квартира',
   'Неуютное бунгало по колено в воде'
 ];
 
-var HOTEL_TYPE = ['palace',
+var HOTEL_TYPE = [
+  'palace',
   'flat',
   'house',
   'bungalo'
 ];
-var HOTEL_CHECKIN = ['12:00',
+var HOTEL_CHECKIN = [
+  '12:00',
   '13:00',
   '14:00'
 ];
 
-var HOTEL_CHECKOUT = ['12:00',
+var HOTEL_CHECKOUT = [
+  '12:00',
   '13:00',
   '14:00'
 ];
 
-var HOTEL_FEATURES = ['wifi',
+var HOTEL_FEATURES = [
+  'wifi',
   'dishwasher',
   'parking',
   'washer',
@@ -33,11 +49,11 @@ var HOTEL_FEATURES = ['wifi',
   'conditioner'
 ];
 
-var HOTEL_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+var HOTEL_PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
-
 var getHotel = function () {
   var hotel = {
     'author': {
@@ -53,7 +69,7 @@ var getHotel = function () {
       'guests': '',
       'checkin': '',
       'checkout': '',
-      'features': '',
+      'features': [''],
       'description': '',
       'photos': '',
     },
@@ -64,33 +80,51 @@ var getHotel = function () {
     },
   };
 
-  hotel.author.avatar = 'img/avatars/user0' + (i + 1) + '.png';
-  hotel.offer.title = HOTEL_TITLE[i];
-  // hotel.offer.address = hotel.location.x + ', ' + hotel.location.y;
-  hotel.offer.price = getRandomInt(1000, 100000);
-  hotel.offer.type = HOTEL_TYPE[getRandomArray(HOTEL_TYPE)];
-  hotel.offer.rooms = getRandomInt(1, 5);
-  hotel.offer.checkin = HOTEL_CHECKIN[getRandomArray(HOTEL_CHECKIN)];
-  hotel.offer.checkout = HOTEL_CHECKOUT[getRandomArray(HOTEL_CHECKOUT)];
-  // hotel.offer.features = HOTEL_FEATURES[getRandomInt(getRandomArray(HOTEL_FEATURES), getRandomArray(HOTEL_FEATURES))];
-  // hotel.offer.photos = HOTEL_PHOTOS.sort(HOTEL_PHOTOS);
-  hotel.location.y = getRandomInt(130, 630);
+  var hotelFeatures = [];
+  for (var j = 0; j < getRandomNumber(0, HOTEL_FEATURES.length); j++) {
+    hotelFeatures[j] = HOTEL_FEATURES[getRandomElementArray(HOTEL_FEATURES)];
+  }
 
+  hotel.author.avatar = 'img/avatars/user' + AVATAR_NUMBER[i] + '.png';
+  hotel.offer.title = HOTEL_TITLE[i];
+  hotel.location.y = getRandomNumber(130, 630);
+  hotel.offer.address = hotel.location.x + ', ' + hotel.location.y;
+  hotel.offer.price = getRandomNumber(1000, 100000);
+  hotel.offer.type = HOTEL_TYPE[getRandomElementArray(HOTEL_TYPE)];
+  hotel.offer.rooms = getRandomNumber(1, 5);
+  hotel.offer.guests = getRandomNumber(0, 10000000);
+  hotel.offer.checkin = HOTEL_CHECKIN[getRandomElementArray(HOTEL_CHECKIN)];
+  hotel.offer.checkout = HOTEL_CHECKOUT[getRandomElementArray(HOTEL_CHECKOUT)];
+  hotel.offer.features = getUniqueArray(hotelFeatures);
+  hotel.offer.photos = hotelPhotos;
   return hotel;
 };
+var hotelPhotos;
 
 var hotels = [];
 for (var i = 0; i < 8; i++) {
+  hotelPhotos = HOTEL_PHOTOS.sort(compareRandom);
   hotels[i] = getHotel();
 }
 
-function getRandomInt(min, max) {
+function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function getRandomArray(arr) {
+function getRandomElementArray(arr) {
   return Math.floor(Math.random() * arr.length);
 }
 
-console.log(getHotel());
-console.log(hotels);
+function compareRandom(a, b) {
+  return Math.random() - 0.5;
+}
+
+function getUniqueArray(arr) {
+  var obj = {};
+  for (var k = 0; k < arr.length; k++) {
+    var str = arr[k];
+    obj[str] = true;
+  }
+  return Object.keys(obj);
+}
+
